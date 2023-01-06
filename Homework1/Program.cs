2ed18c668374a221dc3d45154f0dd59a2916c9de
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-07 02:14:57 +0700

    Решение Задачи 2 в папке Homework1

---
+++ Homework1/Program.cs
@@ -0,0 +1,24 @@
+﻿//int a = 5;
+//int b = 7;
+
+//int a = 2;
+//int b = 10;
+
+int a = -9;
+int b = -3;
+
+
+if (a > b)
+{
+Console.Write("max=");
+Console.WriteLine(a);
+Console.Write("min=");
+Console.WriteLine(b);
+}
+else
+{
+Console.Write("max=");
+Console.WriteLine(b);
+Console.Write("min=");
+Console.WriteLine(a);
+}
