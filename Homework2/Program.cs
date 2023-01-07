f972a53aac635e1364b838b461294ee9d4b2ff27
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-07 22:29:29 +0700

    Создала папку Homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Ведите переменную а: ");
+int a = Int32.Parse(Console.ReadLine());
+Console.Write("Ведите переменную b: ");
+int b = Int32.Parse(Console.ReadLine());
+Console.Write("Ведите переменную c: ");
+int c = Int32.Parse(Console.ReadLine());
+int max = a;
+
+if (a > max) max=a;
+if (b > max) max=b;
+if (c > max) max=c;
+
+Console.Write("max=");
+Console.WriteLine(max);
