a70e8b3aadfcb0b0d031084d3e190b17b327aab2
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-07 22:52:28 +0700

    Создала папку Homework3

---
+++ Homework3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ведите переменную а: ");
+int a = Int32.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+     {
+        Console.WriteLine(a + "- четное число: ");
+     }
+     else
+     {
+        Console.WriteLine(a + "- нечетное число: ");
+     }
