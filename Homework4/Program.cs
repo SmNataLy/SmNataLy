f59be3c8ee7acee0db28c7d23615cd0551e8eb73
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-08 19:14:50 +0700

    Дописала код и блок-схему в Homework4

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Ведите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int n=1; n<=N; n++)
+    if (n % 2 == 0)
+       Console.WriteLine(n);
