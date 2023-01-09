437cbb957f41416b6e6b8d32af8890825af1c952
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-10 02:06:15 +0700

    Создала папку Algorithms5 и пытаюсь решить задачу

---
+++ Algorithms5/Program.cs
@@ -0,0 +1,16 @@
+﻿// ####  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int number = 34628;
+
+if (number < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    while (number >= 100)
+    {
+        number = number % 100;
+        Console.WriteLine(number);
+    }    
+}
+Console.WriteLine(number % 100);
