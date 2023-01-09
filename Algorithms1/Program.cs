55d9feaddf5ec038410c007785a83d30f6147313
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-09 16:31:13 +0700

    Решаю задачу (пишу код)

--- Algorithms1/Program.cs
+++ Algorithms1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {1, 12, 31, 4, 15, 16, 17, 18};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{ 
+    if(array[index[] == find])
+    {
+        Console.WriteLine(index);
+    }
+    index ++;
+}
+    
+//int[] array = new int[3];
+//void FillArray(int[] collection);
+//{
+    //int length = collection.length;
+    //int index = 0;
+    //while (index < length)
+    //{
+       // collection[index] = newRandom().Next(1,10);
+       // index ++;
+   // }
+//}
+// void PrintArray(int[]col)
