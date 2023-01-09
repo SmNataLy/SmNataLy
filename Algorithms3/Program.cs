6da1a90cfca485a007393c27bc30575db1746375
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-09 23:37:45 +0700

    Создала папку Algorithms3 с задачей по семинару

---
+++ Algorithms3/Program.cs
@@ -0,0 +1,45 @@
+﻿// на входе получаем массив на выходе создть новый массив, который заполняется числами кратными 5
+// метод создания массива
+
+// метод заполнения массива
+
+// метод подсчета чисел кратных 5
+
+// решение задачи
+
+// метод печати
+
+int[] array = {2, 5, 6, 10, 32, 30 };
+
+int count = 0;
+int index = 0; // объявить переменную
+
+while(index < array.Length)
+{
+    if (array[index] % 5 == 0)
+    {
+        count++;
+    }
+    index++;
+}
+
+int[] arrayNew = new int [count];
+index = 0; // присвоить значение
+int indexNew = 0;
+
+while (index < array.Length)
+{
+    if (array[index] % 5 == 0)
+    {
+        arrayNew[indexNew] = array[index];
+        indexNew++;
+    }
+    index++;
+}
+Console.WriteLine(arrayNew);
+index = 0;
+while (index < arrayNew.Length)
+{
+    System.Console.WriteLine(arrayNew[index]);
+    index++;
+}
