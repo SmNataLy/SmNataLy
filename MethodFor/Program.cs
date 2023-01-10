215c5f2911e66c3b85be3c0e23ed3498daf7608e
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-10 21:24:56 +0700

    Cоздала папку MethodFor цикл в цикле

---
+++ MethodFor/Program.cs
@@ -0,0 +1,47 @@
+﻿//и цикл в цикле
+
+//for (int i = 2; i <= 10; i++)
+//{
+//    for (int j = 2; j <= 10; j++)
+//    {
+//        Console.WriteLine($"{i} * {j} = {i * j}");
+//    }
+//    Console.WriteLine();
+//}
+
+// работа с текстом
+// Дан текст, в нем нужно все пробелы заменить черточками,
+// а маленькие буквы "к" заменить большими "К",
+// а маленькие "С" заменить большими "с"
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty; // инициализация новой строки
+
+    int length = text.Length;
+    for (int i = 2; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
