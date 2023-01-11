4d1c42a1818fa02943fd3dfc877976d53b1251b7
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-11 23:08:45 +0700

    Cоздала папку Atask21 с решением задачи

---
+++ Atask21/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+//A (3,6,8); B (2,1,-7), -> 15.84
+
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double GetDistsnce(double x1, double x2, double y1, double y2, double z1, double z2)
+{
+    //return Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+    return Math.Sqrt(Math.Pow(x1 - x2, 2)
+                    + Math.Pow(y1 - y2, 2)
+                    + Math.Pow(z1 - z2, 2));
+}
+
+double GetValue(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double x1 = GetValue("x1 =");
+double y1 = GetValue("y1 =");
+double z1 = GetValue("z1 =");
+double x2 = GetValue("x2 =");
+double y2 = GetValue("y2 =");
+double z2 = GetValue("z2 =");
+
+
+double distance = GetDistsnce(x1, x2, y1, y2, z1, z2);
+System.Console.WriteLine(distance);
+
