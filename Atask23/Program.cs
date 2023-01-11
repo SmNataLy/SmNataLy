aecfacef615cf169ee5e868e28e8743bbd5b6413
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-12 00:07:37 +0700

    Cоздала папку Atask23 с итоговым кодом

--- Atask23/Program.cs
+++ Atask23/Program.cs
@@ -1,14 +1,12 @@
-﻿//и цикл в цикле
+﻿
 //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-//3 -> 1, 8, 27
-//5 -> 1, 8, 27, 64, 125
 
-for (int i = 2; i <= 10; i++)
+
+Console.Write("Ведите число N: ");
+int endNum = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= endNum; i++)
 {
-    for (int j = 2; j <= 10; j++)
-    {
-        Console.WriteLine($"{i} * {j} = {i * j}");
-    }
-    Console.WriteLine();
-}
+    Console.WriteLine($" {i} * {i} * {i} = {i * i * i}");
 
+}
