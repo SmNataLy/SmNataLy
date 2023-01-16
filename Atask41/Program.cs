ac9f346f2f7d9eeb61ed287bec2c8909029d5086
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-16 15:17:50 +0700

    Создала папку Atask41 (Задача 41)

---
+++ Atask41/Program.cs
@@ -0,0 +1,48 @@
+﻿// 1. Ввод длинны массива М
+
+int GetNumberByUser(string text)
+{
+    Console.Write(text + " ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// 2. Создание и заполнение массива числами c клавиатуры
+
+void FillArray(int[] collection)
+{
+    int index = 0;
+    for (int i = 0; i < collection.Length; i++)
+    {
+        Console.WriteLine("Введите элемент {0}: ", i + 1);
+        int num = Convert.ToInt32(Console.ReadLine());
+        collection[index++] = num;
+    }    
+}
+
+// 4. Печать массива
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int size = GetNumberByUser("Введите длинну массива М: ");
+int[] array = new int[size];
+
+FillArray(array);
+PrintArray(array);
+
+
+
+
+
+
+
+
+
