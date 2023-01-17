5fa725695f9682d490ccdc567e7b156611f7a824
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-18 00:47:09 +0700

    Создала папку Atask47 и скорректировала код в Atask41

--- Atask41/Program.cs
+++ Atask41/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Пользователь вводит с клавиатуры M чисел.
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-
 // 1. Ввод длинны массива М
 
 int GetNumberByUser(string text)
@@ -42,5 +41,34 @@ FillArray(array);
 int result = CountNumbers(array);
 Console.WriteLine(result);
 
+// Код скорректирован с учетом поправок от преподавателя:
+// Тут можно было в одном цикле и получать числа от пользователя и считать положительные числа.
+// Даже можно было их не записывать в массив, в условиях же этого не требовалось:)
 
+// int GetNumberByUser(string text)
+// {
+//     Console.Write(text + " ");
+//     return Convert.ToInt32(Console.ReadLine());
+// }
 
+// int CountNumbers(int[] collection)
+// {
+  
+//     int result = 0;
+//     for (int i = 0; i < collection.Length; i++)
+//     {
+//         Console.WriteLine("Введите элемент {0}: ", i + 1);
+//         int num = Convert.ToInt32(Console.ReadLine());
+//         if (num > 0)
+//         {
+//             result = result + 1;
+//         }
+        
+//     }
+//     return result;
+// }
+
+// int size = GetNumberByUser("Введите длинну массива М: ");
+// int[] array = new int[size];
+// int result = CountNumbers(array);
+// Console.WriteLine(result);
---
+++ Atask47/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int GetNumberByUser(string text)
+{
+    Console.Write(text + " ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountNumbers(int[] collection)
+{
+  
+    int result = 0;
+    for (int i = 0; i < collection.Length; i++)
+    {
+        Console.WriteLine("Введите элемент {0}: ", i + 1);
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num > 0)
+        {
+            result = result + 1;
+        }
+        
+    }
+    return result;
+}
+
+int size = GetNumberByUser("Введите длинну массива М: ");
+int[] array = new int[size];
+int result = CountNumbers(array);
+Console.WriteLine(result);
+
