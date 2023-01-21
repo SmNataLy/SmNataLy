f0872d93a1ea7fc109fad6c57567f587c749d50d
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-21 23:22:12 +0700

    Создала папку Atask68 корректировка кода

--- Atask68/Program.cs
+++ Atask68/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-double A(double m, double n)
+double FunctionA(double m, double n)
 {
     if (m == 0)
         return n + 1;
     else if (m > 0 && n == 0)
-        return A(m - 1, 1);
-    else return A(m - 1, A(m, n - 1));
+        return FunctionA(m - 1, 1);
+    else return FunctionA(m - 1, FunctionA(m, n - 1));
 }
-Console.WriteLine(A(3, 2));
+Console.WriteLine(FunctionA(2, 3));
