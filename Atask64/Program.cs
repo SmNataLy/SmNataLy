71c156b77c34e467873f93e7135e426016ab37cf
SmNataLy <121353498+SmNataLy@users.noreply.github.com>
2023-01-21 18:06:47 +0700

    Создала папку Atask64 c кодом решения и Atask66 без решения

---
+++ Atask64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии
+
+int GetNumberByUser(string text)
+{
+    Console.Write(text + " ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int firstNum = GetNumberByUser("Bведите значение N: ");
+
+int lastNum = 1;
+string NumbersRec(int lastNum, int firstNum)
+{
+    if (lastNum <= firstNum) return NumbersRec(lastNum + 1, firstNum) + $"{lastNum}, ";
+    else return String.Empty;
+}
+string number = NumbersRec(lastNum, firstNum);
+System.Console.WriteLine(number);
